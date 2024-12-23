3cad7115327860e5c5090aecb522c2652f20a472
Volkov Pavel <volkov.pa@promail366.com>
2024-12-23 11:27:24 +0200

    refactor for 2 func send message and 2 handlers

--- main.go
+++ main.go
@@ -16,36 +16,24 @@ func calculateDaysSince(targetDate time.Time) int {
 	return int(diff.Hours() / 24)
 }
 
-func calculateDaysSinceSokr(sokrDate time.Time)int {
-	currentDate := time.Now()
-	diff := currentDate.Sub(sokrDate)
-	return int(diff.Hours() / 24)
-}
-
-func sendMessage(bot *telebot.Bot, targetDate time.Time, chatID int64) {
+func sendMessage(bot *telebot.Bot, targetDate time.Time, messageText string, chatID int64) {
 	daysSince := calculateDaysSince(targetDate)
-	message := fmt.Sprintf("Маша не выходит замуж %d дней", daysSince)
+	message := fmt.Sprintf(messageText, daysSince)
 
 	bot.Send(&telebot.Chat{ID: chatID}, message)
 }
-func sendMessage(bot *telebot.Bot, sokrDate time.Time, chatID int64) {
-	daysSince := calculateDaysSince(sokrDate)
-	message2 := fmt.Sprintf("Дилару не сокращают %d дней", daysSince)
 
-	bot.Send(&telebot.Chat{ID: chatID}, message2)
-}
 func main() {
-	token := os.Getenv("TELEGRAM_BOT_TOKEN")   // Токен  бота
-	chatIDStr := os.Getenv("TELEGRAM_CHAT_ID") // ID чата, в который бот будет отправлять сообщения
-
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")   
+	chatIDStr := os.Getenv("TELEGRAM_CHAT_ID") 
 	chatID, err := strconv.ParseInt(chatIDStr, 10, 64)
 	if err != nil {
 		fmt.Println("Error parsing chat ID:", err)
 		return
 	}
 
-	targetDate := time.Date(2020, time.January, 6, 0, 0, 0, 0, time.UTC)
-	sokrDate := time.Date(2024, time.December, 19, 0, 0, 0, 0, time.UTC)
+	targetDate := time.Date(2020, time.January, 6, 0, 0, 0, 0, time.UTC) 
+	sokrDate := time.Date(2024, time.December, 19, 0, 0, 0, 0, time.UTC) 
 
 	bot, err := telebot.NewBot(telebot.Settings{
 		Token:  token,
@@ -60,21 +48,19 @@ func main() {
 	bot.Handle("/skolko", func(c telebot.Context) error {
 		daysSince := calculateDaysSince(targetDate)
 		message := fmt.Sprintf("Маша не выходит замуж %d дней", daysSince)
-
 		return c.Send(message)
 	})
-	
+
 	bot.Handle("/sokr", func(c telebot.Context) error {
-		daysSince := calculateDaysSinceSokr(sokrDate)
+		daysSince := calculateDaysSince(sokrDate)
 		message := fmt.Sprintf("Дилару не сокращают %d дней", daysSince)
-
 		return c.Send(message)
 	})
 
-	c := cron.New(cron.WithLocation(time.FixedZone("MSK", 3*60*60))) // Московское время
-
+	c := cron.New(cron.WithLocation(time.FixedZone("MSK", 3*60*60))) 
 	_, err = c.AddFunc("0 12 * * *", func() {
-		sendMessage(bot, targetDate, chatID)
+		sendMessage(bot, targetDate, "Маша не выходит замуж %d дней", chatID)
+		sendMessage(bot, sokrDate, "Дилару не сокращают %d дней", chatID)
 	})
 
 	if err != nil {
